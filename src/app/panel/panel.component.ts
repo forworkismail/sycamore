7ea7ed54aed32ea10dcdf57a43907bb57b0a956b
Mohammad Ismail <forwork.ismail@gmail.com>
2023-10-06 12:08:49 +0430

    refactor: change panel-shell to panel component
    On branch main
    Changes to be committed:
    	deleted:    src/app/panel/panel-shell.component.html
    	new file:   src/app/panel/panel.component.html
    	renamed:    src/app/panel/panel-shell.component.ts -> src/app/panel/panel.component.ts

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,50 +1,53 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { Subscription, filter } from 'rxjs';
 import { TextTitleComponent } from '../common/components/text-title/text-title.component';
-import { OverlayComponent } from '../common/components/overlay/overlay.component';
-import { SidebarIconComponent } from '../common/icons/sidebar-icon/sidebar-icon.component';
+import { SidebarIconComponent } from '../common/icons/sidebar-icon.component';
 import { DrawerComponent } from '../common/components/drawer/drawer.component';
-import { SidebarComponent } from '../common/components/sidebar/sidebar.component';
+import { SidebarComponent } from './ui/sidebar/sidebar.component';
+import { CompanyIconComponent } from '../common/icons/company-icon.component';
 
 @Component({
-  selector: 'app-panel-shell',
+  selector: 'app-panel',
   standalone: true,
-  templateUrl: './panel-shell.component.html',
+  templateUrl: './panel.component.html',
   imports: [
     CommonModule,
     TextTitleComponent,
     RouterOutlet,
-    OverlayComponent,
     SidebarIconComponent,
     DrawerComponent,
     SidebarComponent,
+    CompanyIconComponent,
   ],
 })
-export default class PanelShellComponent {
+export default class PanelComponent {
+  @ViewChild('triggerButton', { static: true }) triggerButton: ElementRef = {} as ElementRef;
   activeRoute = '';
   routerSubscription = new Subscription();
   toggleSidebar = false;
-  count = signal(0);
 
   private router = inject(Router);
+  // private overlayService = inject(OverlayService);
 
   ngOnInit() {
     this.getActiveRouteSegment();
     this.routerSubscription = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
       this.getActiveRouteSegment();
     });
-    console.log('The count is: ' + this.count());
   }
+
   getActiveRouteSegment(): void {
     const urlSegments = this.router.url.split('/');
-    this.activeRoute = urlSegments[1];
+    this.activeRoute = urlSegments[2] || urlSegments[1];
   }
+
   onToggleSidebar() {
     this.toggleSidebar = !this.toggleSidebar;
   }
-  increase() {
-    this.count.update(value => value + 1);
-  }
+
+  // showOverlay(): void {
+  //   this.overlayService.showOverlayForComponent(this.triggerButton);
+  // }
 }
