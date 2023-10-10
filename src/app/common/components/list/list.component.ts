94b5fb1533cc152cef2fd2506e1554c1ff3d28f6
Mohammad Ismail <forwork.ismail@gmail.com>
2023-10-10 16:47:14 +0430

    refactor: show data after 2 seconds delay
    Changes to be committed:
    	modified:   src/app/common/components/list/list.component.html
    	modified:   src/app/common/components/list/list.component.ts

--- src/app/common/components/list/list.component.ts
+++ src/app/common/components/list/list.component.ts
@@ -21,93 +21,105 @@ import { EyeIconComponent } from '../../icons/eye-icon.component';
     TrashIconComponent,
     EllipsisIconComponent,
     EyeIconComponent,
+    NgIf,
   ],
 })
 export class ListComponent {
   constructor(public dir: Directionality) {}
-  items = Array.from({ length: 20 });
+  items = [];
+  headers: any = [];
 
-  headers = [
-    {
-      label: 'id',
-      mapping: 'id',
-      show: false,
-      dependsOn: ['id'],
-    },
-    {
-      label: 'Name',
-      mapping: 'customerName',
-      show: true,
-      class: 'min-w-[250px] max-w-[250px]',
-      dependsOn: ['firstName', 'lastName'],
-    },
-    {
-      label: 'Company',
-      mapping: 'company',
-      show: true,
-      class: 'min-w-[150px] max-w-[150px]',
-      dependsOn: ['company'],
-    },
-    {
-      label: 'Email',
-      mapping: 'email',
-      show: true,
-      class: 'min-w-[150px] max-w-[150px]',
-      dependsOn: ['email'],
-    },
-    {
-      label: 'Payment',
-      mapping: 'paymentMethod',
-      show: true,
-      class: 'min-w-[150px] max-w-[150px] flex flex-row items-center',
-      dependsOn: ['cardType', 'cardNumber'],
-    },
-    {
-      label: 'Created Date',
-      mapping: 'createdDate',
-      show: true,
-      class: 'min-w-[150px] max-w-[150px] truncate',
-      dependsOn: ['createdDate'],
-    },
-    {
-      label: 'Mobile',
-      mapping: 'mobile',
-      show: false,
-      dependsOn: ['mobile'],
-    },
-    {
-      label: 'Address',
-      mapping: 'address',
-      show: false,
-      dependsOn: ['address'],
-    },
-    {
-      label: 'Home',
-      mapping: 'home',
-      show: false,
-      dependsOn: ['home'],
-    },
-    {
-      label: 'Business',
-      mapping: 'business',
-      show: false,
-      dependsOn: ['business'],
-    },
-    {
-      label: 'Fax',
-      mapping: 'fax',
-      show: false,
-      class: 'm-w-[140px]',
-      dependsOn: ['fax'],
-    },
-    {
-      label: 'Notes',
-      mapping: 'notes',
-      show: false,
-      class: 'm-w-[140px]',
-      dependsOn: ['notes'],
-    },
-  ];
+  // after 2 seconds fetch headers
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.items = Array.from({ length: 20 });
+      this.fetchHeaders();
+    }, 2000);
+  }
+
+  fetchHeaders() {
+    this.headers = [
+      {
+        label: 'id',
+        mapping: 'id',
+        show: false,
+        dependsOn: ['id'],
+      },
+      {
+        label: 'Name',
+        mapping: 'customerName',
+        show: true,
+        class: 'min-w-[250px] max-w-[250px]',
+        dependsOn: ['firstName', 'lastName'],
+      },
+      {
+        label: 'Company',
+        mapping: 'company',
+        show: true,
+        class: 'min-w-[150px] max-w-[150px]',
+        dependsOn: ['company'],
+      },
+      {
+        label: 'Email',
+        mapping: 'email',
+        show: true,
+        class: 'min-w-[150px] max-w-[150px]',
+        dependsOn: ['email'],
+      },
+      {
+        label: 'Payment',
+        mapping: 'paymentMethod',
+        show: true,
+        class: 'min-w-[150px] max-w-[150px] flex flex-row items-center',
+        dependsOn: ['cardType', 'cardNumber'],
+      },
+      {
+        label: 'Created Date',
+        mapping: 'createdDate',
+        show: true,
+        class: 'min-w-[150px] max-w-[150px] truncate',
+        dependsOn: ['createdDate'],
+      },
+      {
+        label: 'Mobile',
+        mapping: 'mobile',
+        show: false,
+        dependsOn: ['mobile'],
+      },
+      {
+        label: 'Address',
+        mapping: 'address',
+        show: false,
+        dependsOn: ['address'],
+      },
+      {
+        label: 'Home',
+        mapping: 'home',
+        show: false,
+        dependsOn: ['home'],
+      },
+      {
+        label: 'Business',
+        mapping: 'business',
+        show: false,
+        dependsOn: ['business'],
+      },
+      {
+        label: 'Fax',
+        mapping: 'fax',
+        show: false,
+        class: 'm-w-[140px]',
+        dependsOn: ['fax'],
+      },
+      {
+        label: 'Notes',
+        mapping: 'notes',
+        show: false,
+        class: 'm-w-[140px]',
+        dependsOn: ['notes'],
+      },
+    ];
+  }
   abc() {
     event?.stopPropagation();
     console.log('ABC');
