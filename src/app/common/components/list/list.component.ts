60dfab949bf7b1c17eeb532cf7edb3889ce27653
Mohammad Ismail <forwork.ismail@gmail.com>
2023-10-19 22:07:12 +0430

    fix: resolve list to work with table column mapper
    Changes to be committed:
    	modified:   src/app/common/components/list/list.component.html
    	modified:   src/app/common/components/list/list.component.ts

--- src/app/common/components/list/list.component.ts
+++ src/app/common/components/list/list.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule, NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { NgClass, NgFor, NgIf, NgStyle, TitleCasePipe } from '@angular/common';
 import { ScrollableAreaComponent } from '../base/scrollable-area/scrollable-area.component';
 import { ButtonComponent } from '../button/button.component';
 import { TrashIconComponent } from 'app/common/icons/trash-icon.component';
 import { EllipsisIconComponent } from 'app/common/icons/ellipsis-icon.component';
 import { Directionality } from '@angular/cdk/bidi';
 import { EyeIconComponent } from '../../icons/eye-icon.component';
+import { TableColumn } from 'app/common/store/table/table.state';
+import { Product } from 'app/panel/feature/products/data-access/store/product.state';
 
 @Component({
   selector: 'app-list',
@@ -14,8 +16,9 @@ import { EyeIconComponent } from '../../icons/eye-icon.component';
   styleUrls: ['./list.component.css'],
   imports: [
     NgFor,
+    NgStyle,
     NgClass,
-    NgIf,
+    TitleCasePipe,
     ScrollableAreaComponent,
     ButtonComponent,
     TrashIconComponent,
@@ -24,104 +27,15 @@ import { EyeIconComponent } from '../../icons/eye-icon.component';
     NgIf,
   ],
 })
-export class ListComponent {
+export class ListComponent<T> {
+  @Input() columns: TableColumn<T>[] = [];
+  @Input() rows: T[] = [];
+
   constructor(public dir: Directionality) {}
-  items = [];
-  headers: any = [];
 
-  // after 2 seconds fetch headers
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.items = Array.from({ length: 20 });
-      this.fetchHeaders();
-    }, 2000);
-  }
-
-  fetchHeaders() {
-    this.headers = [
-      {
-        label: 'id',
-        mapping: 'id',
-        show: false,
-        dependsOn: ['id'],
-      },
-      {
-        label: 'Name',
-        mapping: 'customerName',
-        show: true,
-        class: 'min-w-[250px] max-w-[250px]',
-        dependsOn: ['firstName', 'lastName'],
-      },
-      {
-        label: 'Company',
-        mapping: 'company',
-        show: true,
-        class: 'min-w-[150px] max-w-[150px]',
-        dependsOn: ['company'],
-      },
-      {
-        label: 'Email',
-        mapping: 'email',
-        show: true,
-        class: 'min-w-[150px] max-w-[150px]',
-        dependsOn: ['email'],
-      },
-      {
-        label: 'Payment',
-        mapping: 'paymentMethod',
-        show: true,
-        class: 'min-w-[150px] max-w-[150px] flex flex-row items-center',
-        dependsOn: ['cardType', 'cardNumber'],
-      },
-      {
-        label: 'Created Date',
-        mapping: 'createdDate',
-        show: true,
-        class: 'min-w-[150px] max-w-[150px] truncate',
-        dependsOn: ['createdDate'],
-      },
-      {
-        label: 'Mobile',
-        mapping: 'mobile',
-        show: false,
-        dependsOn: ['mobile'],
-      },
-      {
-        label: 'Address',
-        mapping: 'address',
-        show: false,
-        dependsOn: ['address'],
-      },
-      {
-        label: 'Home',
-        mapping: 'home',
-        show: false,
-        dependsOn: ['home'],
-      },
-      {
-        label: 'Business',
-        mapping: 'business',
-        show: false,
-        dependsOn: ['business'],
-      },
-      {
-        label: 'Fax',
-        mapping: 'fax',
-        show: false,
-        class: 'm-w-[140px]',
-        dependsOn: ['fax'],
-      },
-      {
-        label: 'Notes',
-        mapping: 'notes',
-        show: false,
-        class: 'm-w-[140px]',
-        dependsOn: ['notes'],
-      },
-    ];
-  }
-  abc() {
-    event?.stopPropagation();
-    console.log('ABC');
+  getColumnWidthClass(column: TableColumn<T>, totalColumns: number): string {
+    const fraction = column.width ? column.width : 1 / totalColumns;
+    const percentage = fraction * 100;
+    return `${percentage}%`;
   }
 }
